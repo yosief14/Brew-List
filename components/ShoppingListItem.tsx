253dc364ae6975a93952d36bf793e58555a3d5ab
Yosief Gebremedhin <yosief10@outlook.com>
2024-01-13 19:34:32 -0700

    Add ShoppingListContextProvider and ShoppingListItem components

---
+++ components/ShoppingListItem.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { Input, CheckBox } from '@rneui/themed';
+import { Text, Pressable } from 'react-native';
+import { useContext } from 'react';
+import { ShoppingListContext } from '../context/ShoppingListContextProvider';
+import { XStack, YGroup, Button, YStack } from 'tamagui';
+import { Check as CheckIcon, Minus, Plus } from '@tamagui/lucide-icons';
+
+export default function ShoppingListItem(props: { name: string, quantity: number, lowQuantity: number, targetQuantity: number, location: string }) {
+    const [state, dispatch] = useContext(ShoppingListContext)
+    console.log("🚀 ~ ShoppingListItem ~ state:", state)
+    const item = state.items[props.name] || { quantity: props.quantity, checked: false };
+    function changeQuantity(amount: number) {
+        if (item.quantity + amount >= 0)
+            dispatch({ type: 'setQuantity', name: props.name, quantity: item.quantity + amount });
+    }
+    const toggleChecked = () => { dispatch({ type: 'setChecked', name: props.name, checked: !item.checked }) };
+
+
+    return (
+        //TODO - Style title and quantity so that they are further apart
+        <XStack display="flex" alignItems='center' justifyContent={"space-between"} width={"90%"} borderRadius={10} backgroundColor={"#1e1e24"} height={120} paddingHorizontal={10} paddingVertical={10} opacity={item.checked ? 0.5 : 1}>
+            <CheckBox checked={item.checked} onPress={toggleChecked} iconType="material-community"
+                checkedIcon="checkbox-outline"
+                uncheckedIcon={'checkbox-blank-outline'}
+                size={32}
+                checkedColor='#23A24D'
+                uncheckedColor='white'
+                containerStyle={{ backgroundColor: "#1e1e24" }}
+            >
+            </CheckBox>
+            <YStack flexGrow={1} height={"100%"} alignItems='center' justifyContent='space-between' >
+                <Text style={{ fontSize: 20, fontWeight: "bold", color: "white" }}>
+                    {props.name}
+                </Text>
+                <Input containerStyle={{ maxWidth: 60, height: 60, padding: 0, margin: 0, justifyContent: "center", flexDirection: "row" }}
+                    inputContainerStyle={{ borderBottomWidth: 0, flexDirection: "column", backgroundColor: "#111d4a", borderRadius: 60, width: 60, height: 60, elevation: 10, shadowOffset: { width: 0, height: 0 }, shadowColor: "white", shadowOpacity: 0.5, shadowRadius: 5 }}
+                    inputStyle={{ fontWeight: "bold", fontSize: 15, color: "white" }}
+                    returnKeyType='done'
+                    inputMode='decimal'
+                    defaultValue={item.quantity.toString()}
+                >
+                </Input>
+            </YStack>
+
+            <YGroup justifyContent='center' height={"100%"} width={60}>
+                <Button icon={Plus} fontWeight={"bold"} flexGrow={1} color={"white"} backgroundColor={"#111d4a"} scaleIcon={2} size="$2" onPress={() => changeQuantity(1)} disabled={item.checked} />
+                <Button icon={Minus} fontWeight={"bold"} color={"white"} backgroundColor={"black"} flexGrow={1} scaleIcon={2} size="$2" onPress={() => changeQuantity(-1)} />
+                {/*TODO - wrap this input in a Pressable that sets the focus to the input*/}
+
+            </YGroup>
+
+
+        </XStack >
+    )
+
+
+}
---
+++ context/ShoppingListContextProvider.tsx
@@ -0,0 +1,46 @@
+import React, { createContext, useReducer, useContext } from 'react';
+
+// ? Creates a State object
+type State = {
+    items: { [key: string]: { quantity: number, checked: boolean } };
+};
+
+// ? Creates a Action object
+type Action =
+    | { type: 'setQuantity'; name: string; quantity: number }
+    | { type: 'setChecked'; name: string; checked: boolean };
+
+const initialState: State = { items: {} };
+
+function reducer(state: State, action: Action): State {
+    switch (action.type) {
+        case 'setQuantity':
+            return { ...state, items: { ...state.items, [action.name]: { ...state.items[action.name], quantity: action.quantity } } };
+        case 'setChecked':
+            return { ...state, items: { ...state.items, [action.name]: { ...state.items[action.name], checked: action.checked } } };
+        default:
+            return state;
+    }
+}
+
+// ? Creates a Context object 
+// ? The context object contains a state and a dispatch function
+// ? The context object is initialized with the initialState and an empty dispatch function
+// ? The Dispatch function is a type definityion usid in conjucation with useReducer
+// ? useReducer is a allternative to useState that is preferable when complex state logic
+//TODO - Figure out why when I pass initialState.items to createContext it works but when I pass just initialState it doesn't
+export const ShoppingListItemsContext = createContext(initialState);
+export const ShoppingListDispatchContext = createContext<React.Dispatch<Action>>(null);
+
+//TODO revisit dispatch
+export function ShoppingListContextProvider({ children }) {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    return (
+        <ShoppingListItemsContext.Provider value={state}>
+            <ShoppingListDispatchContext.Provider value={dispatch}>
+                {children}
+            </ShoppingListDispatchContext.Provider>
+        </ShoppingListItemsContext.Provider>
+    );
+}
+
