bb49d46b106448cf64b3247b7a22aa48112c88ec
Yosief Gebremedhin <yosief10@outlook.com>
2024-01-29 12:04:53 -0700

    Add screen width to Accordion component

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import { ListItem } from '@rneui/themed';
 
 import { XStack, YStack } from 'tamagui';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { List } from '@tamagui/lucide-icons';
 
+const Screen = Dimensions.get("screen").width;
 export default function Accordion({ titleID, children }) {
     const [isExpanded, setIsExpanded] = React.useState(false);
 
@@ -14,24 +15,22 @@ export default function Accordion({ titleID, children }) {
 
 
     return (
-        <>
-            <ListItem.Accordion
-                content={
-                    <ListItem.Content>
-                        <ListItem.Title><Text style={{ fontSize: 20, fontWeight: "bold" }}>{titleID}</Text></ListItem.Title>
-                    </ListItem.Content>}
-                isExpanded={isExpanded}
-                onPress={() => setIsExpanded(!isExpanded)}
-                style={{ backgroundColor: "red" }}
-                containerStyle={{
-                    width: "90%", backgroundColor: "white", borderRadius: 10, gap: 10
-                }}
-            >
-                <YStack width={"60%"} gap={10}>
-                    {children}
-                </YStack>
-            </ListItem.Accordion >
-        </>
+        <ListItem.Accordion
+            content={
+                <ListItem.Content>
+                    <ListItem.Title><Text style={{ fontSize: 20, fontWeight: "bold", color: "white" }}>{titleID}</Text></ListItem.Title>
+                </ListItem.Content>}
+            isExpanded={isExpanded}
+            onPress={() => setIsExpanded(!isExpanded)}
+            containerStyle={{
+                width: "90%", backgroundColor: "#1e1e24", borderRadius: 10, gap: 10, borderColor: colors['Raisin black'], borderWidth: 1
+            }}
+        >
+            <YStack width={Screen} alignItems={"center"} gap={10} >
+                {children}
+            </YStack>
+        </ListItem.Accordion >
+
     )
 
 }
